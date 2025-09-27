3ac2e3f83ec6a9bacd268c8d7630d0d8b603bed5
Jose Maximiliano Diaz Mendez <josemaximilianodiazmendez@gmail.com>
2025-09-27 01:30:43 -0600

    feat(useFetchCareers): add support for search and order

--- composables/useFetchCareers.ts
+++ composables/useFetchCareers.ts
@@ -1,14 +1,33 @@
+import type { MaybeRef } from 'vue'
 import type { H3Error } from 'h3'
 import type { Career } from '~/types/api/career.d'
 import { useRuntimeConfig } from '#imports'
 import type { FastifyError } from '~/types/api/error.d'
 
-export const useFetchCareers = () => {
+interface UseFetchCareersParams {
+  search?: MaybeRef<string | undefined>
+  order?: MaybeRef<string>
+}
+
+export const key = 'careers-list'
+
+export const useFetchCareers = (params: UseFetchCareersParams = {}) => {
   const config = useRuntimeConfig()
+
+  const {
+    search = undefined,
+    order = '-Careers.createdAt'
+  } = params
+
+  const query = computed(() => ({
+    search: unref(search),
+    order: unref(order)
+  }))
 
   const { data, status, error, refresh, pending } = useFetch<Career[], H3Error<FastifyError>>('/careers', {
     baseURL: config.public.serverHost,
-    key: 'careers-list'
+    key,
+    query
   })
 
   return {
