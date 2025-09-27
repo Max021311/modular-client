e30fc317a3c683e851c5c692e7bb8762aac156b7
Jose Maximiliano Diaz Mendez <josemaximilianodiazmendez@gmail.com>
2025-09-27 01:24:20 -0600

    feat(Composables): add `useAddCareer`

---
+++ composables/useAddCareer.ts
@@ -0,0 +1,49 @@
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+import { isAxiosError } from 'axios'
+import type { FastifyError } from '~/types/api/error.d'
+import type { CreateCareer, Career } from '~/types/api/career.d'
+import useAxios from '~/composables/useAxios'
+import { key as careersListKey } from '~/composables/useFetchCareers'
+
+export const useAddCareer = () => {
+  const axios = useAxios()
+
+  const error: Ref<Error | FastifyError | null> = ref(null)
+  const pending = ref(false)
+  const data: Ref<Career | null> = ref(null)
+
+  const mutate = async (payload: CreateCareer, token: string): Promise<void> => {
+    try {
+      pending.value = true
+      error.value = null
+      data.value = null
+
+      const response = await axios.post<Career>('/careers', payload, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+
+      data.value = response.data
+      refreshNuxtData(careersListKey)
+    } catch (err: unknown) {
+      if (isAxiosError<FastifyError>(err) && err.response) {
+        error.value = err.response.data
+      } else if (err instanceof Error) {
+        error.value = err
+      } else {
+        error.value = new Error('Unknown error')
+      }
+    } finally {
+      pending.value = false
+    }
+  }
+
+  return {
+    error,
+    pending,
+    data,
+    mutate
+  }
+}
